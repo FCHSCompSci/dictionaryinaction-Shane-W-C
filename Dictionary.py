9ce4a6a6eb0db8987745c8775225e0422afc2e4b
Shane Wesley Callahan <shane.callahan@stu.jefferson.kyschools.us>
2018-09-18 10:35:47 -0400

    Started on project

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+import time
+
+leaders = {
+    'People': "Aaron Kimball",
+    'Elites': "The Barons",
+    'Ruler': "Allgood Murphy",
+    'Intellectuals': "Grant C. Hayes",
+    }
+
+for k, v in leaders.items():
+    print("%s, %s" %(k, v))
