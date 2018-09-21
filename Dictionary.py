78d47503bee30605e4aca84c7a42d7411c825e6a
Shane Wesley Callahan <shane.callahan@stu.jefferson.kyschools.us>
2018-09-21 10:33:28 -0400

    Actually got the main functions to work!

--- Dictionary.py
+++ Dictionary.py
@@ -11,39 +11,48 @@ coup = ""
 curr_lead = leaders['people']
 new_lead = ""
 
-#This sets up the possible leaders, and their ideals.
+# This sets up the possible leaders, and their ideals.
 
 def possib():
     for k, v in leaders.items():
-        print("%s, %s" %(k, v))
+        print("%s: %s" %(k, v))
         time.sleep(.25)
+
+# Prints the dictionary in a fashionable method.
 
 
 def coup():
     while True:
         new_lead = input()
         if new_lead.lower() == "people":
-            curr_lead = leaders['people']
-            break
+            return leaders['people']
         elif new_lead.lower() == "elites":
-            curr_lead = leaders['elites']
-            break
+            return leaders['elites']
         elif new_lead.lower() == "ruler":
-            curr_lead = leaders['ruler']
-            break
+            return leaders['ruler']
         elif new_lead.lower() == "intel":
-            curr_lead = leaders['intel']
-            break
+            return leaders['intel']
         else:
             print("That's not a choice!")
 
+# Coup function, allows you to switch out who the current leader is.
+
+def pos_coup():
+    print("For this effect to take place, a coup must be held!")
+
+# Tells the 'player' that for the switch to work, they need to host a coup.
+
 def lead():
     print("Current Leader: " + curr_lead)
+
+# Prints the current leader.
 
 def sep():
     print()
     print("-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-")
     print()
+
+# Simple seperator, used a lot.
 
 def switch():
     print("Would you like to change out the leaders? [Y/N]")
@@ -52,24 +61,36 @@ def switch():
         possib()
         print("Who should be changed?")
         ter_op = input()
+        print("Who should be put in charge?")
         switch_two(ter_op)
     else:
         pass
 
+#The first part, where it asks you which party to switch out
+
 
 def switch_two(ter_op):
     if ter_op.lower() == "people":
-        leaders['people'] = input()
+        switch_people = input()
+        leaders.update({'people': switch_people})
+        pos_coup()
     elif ter_op.lower() == "elites":
-        leaders['elites'] = input()
+        switch_elites = input()
+        leaders.update({'elites': switch_elites})
+        pos_coup()
     elif ter_op.lower() == "ruler":
-        leaders['ruler'] = input()
+        switch_ruler = input()
+        leaders.update({'ruler': switch_ruler})
+        pos_coup()
     elif ter_op.lower() == "intel":
-        leaders['intel'] = input()
+        switch_intel = input()
+        leaders.update({'intel': switch_intel})
+        pos_coup()
     else:
         print("That's not an option!")
 
-# Setting up my Functions
+# This is the second part of Switch, where it creates a new name for
+# a current party.
 
 while True:
     time.sleep(.25)
@@ -83,7 +104,7 @@ while True:
         print("Who would you like to put in charge?")
         time.sleep(1)
         possib()
-        coup()
+        curr_lead = coup()
         time.sleep(1)
         print("The coup was successful!")
         sep()
